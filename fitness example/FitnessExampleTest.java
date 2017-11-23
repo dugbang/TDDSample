72b8086352bcaedc932565cd4a5e199583657043
dugbang <dugbang@gmail.com>
2017-11-23 16:53:07 +0900

    20171123

--- fitness example/FitnessExampleTest.java
+++ fitness example/FitnessExampleTest.java
@@ -10,77 +10,77 @@ public class FitnessExampleTest {
     private PageCrawler crawler;
     private WikiPage root;
     private WikiPage testPage;
-    private String expectedResultForTestCase = "<div class=\"setup\">\n" +
-            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\n" +
-            "\t<a href=\"javascript:toggleCollapsable('');\">\n" +
-            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\n" +
-            "\t</a>\n" +
-            "&nbsp;<span class=\"meta\">Set Up: <a href=\"SuiteSetUp\">.SuiteSetUp</a> <a href=\"SuiteSetUp?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\n" +
-            "\t<div class=\"collapsable\" id=\"\">suiteSetUp</div>\n" +
-            "</div>\n" +
-            "<div class=\"setup\">\n" +
-            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\n" +
-            "\t<a href=\"javascript:toggleCollapsable('');\">\n" +
-            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\n" +
-            "\t</a>\n" +
-            "&nbsp;<span class=\"meta\">Set Up: <a href=\"SetUp\">.SetUp</a> <a href=\"SetUp?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\n" +
-            "\t<div class=\"collapsable\" id=\"\">setup</div>\n" +
-            "</div>\n" +
-            "<span class=\"meta\">variable defined: TEST_SYSTEM=slim</span><br/>the content!include -teardown <a href=\"TearDown\">.TearDown</a><br/><div class=\"teardown\">\n" +
-            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\n" +
-            "\t<a href=\"javascript:toggleCollapsable('');\">\n" +
-            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\n" +
-            "\t</a>\n" +
-            "&nbsp;<span class=\"meta\">Tear Down: <a href=\"SuiteTearDown\">.SuiteTearDown</a> <a href=\"SuiteTearDown?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\n" +
-            "\t<div class=\"collapsable\" id=\"\">suiteTearDown</div>\n" +
-            "</div>\n";
+    private String expectedResultForTestCase = "<div class=\"setup\">\r\n" +
+            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\r\n" +
+            "\t<a href=\"javascript:toggleCollapsable('');\">\r\n" +
+            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\r\n" +
+            "\t</a>\r\n" +
+            "&nbsp;<span class=\"meta\">Set Up: <a href=\"SuiteSetUp\">.SuiteSetUp</a> <a href=\"SuiteSetUp?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\r\n" +
+            "\t<div class=\"collapsable\" id=\"\">suiteSetUp</div>\r\n" +
+            "</div>\r\n" +
+            "<div class=\"setup\">\r\n" +
+            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\r\n" +
+            "\t<a href=\"javascript:toggleCollapsable('');\">\r\n" +
+            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\r\n" +
+            "\t</a>\r\n" +
+            "&nbsp;<span class=\"meta\">Set Up: <a href=\"SetUp\">.SetUp</a> <a href=\"SetUp?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\r\n" +
+            "\t<div class=\"collapsable\" id=\"\">setup</div>\r\n" +
+            "</div>\r\n" +
+            "<span class=\"meta\">variable defined: TEST_SYSTEM=slim</span><br/>the content!include -teardown <a href=\"TearDown\">.TearDown</a><br/><div class=\"teardown\">\r\n" +
+            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\r\n" +
+            "\t<a href=\"javascript:toggleCollapsable('');\">\r\n" +
+            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\r\n" +
+            "\t</a>\r\n" +
+            "&nbsp;<span class=\"meta\">Tear Down: <a href=\"SuiteTearDown\">.SuiteTearDown</a> <a href=\"SuiteTearDown?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\r\n" +
+            "\t<div class=\"collapsable\" id=\"\">suiteTearDown</div>\r\n" +
+            "</div>\r\n";
 
-    private String expectedResultForNonTestCase = "<div class=\"setup\">\n"+
-            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\n"+
-            "\t<a href=\"javascript:toggleCollapsable('');\">\n"+
-            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\n"+
-            "\t</a>\n"+
-            "&nbsp;<span class=\"meta\">Set Up: <a href=\"SetUp\">.SetUp</a> <a href=\"SetUp?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\n"+
-            "\t<div class=\"collapsable\" id=\"\">setup</div>\n"+
-            "</div>\n"+
-            "<div class=\"setup\">\n"+
-            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\n"+
-            "\t<a href=\"javascript:toggleCollapsable('');\">\n"+
-            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\n"+
-            "\t</a>\n"+
-            "&nbsp;<span class=\"meta\">Set Up: <a href=\"SuiteSetUp\">.SuiteSetUp</a> <a href=\"SuiteSetUp?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\n"+
-            "\t<div class=\"collapsable\" id=\"\">suiteSetUp</div>\n"+
-            "</div>\n"+
-            "<div class=\"setup\">\n"+
-            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\n"+
-            "\t<a href=\"javascript:toggleCollapsable('');\">\n"+
-            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\n"+
-            "\t</a>\n"+
-            "&nbsp;<span class=\"meta\">Set Up: <a href=\"SetUp\">.SetUp</a> <a href=\"SetUp?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\n"+
-            "\t<div class=\"collapsable\" id=\"\">setup</div>\n"+
-            "</div>\n"+
-            "<span class=\"meta\">variable defined: TEST_SYSTEM=slim</span><br/>the content!include -teardown <a href=\"TearDown\">.TearDown</a><br/><div class=\"teardown\">\n"+
-            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\n"+
-            "\t<a href=\"javascript:toggleCollapsable('');\">\n"+
-            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\n"+
-            "\t</a>\n"+
-            "&nbsp;<span class=\"meta\">Tear Down: <a href=\"SuiteTearDown\">.SuiteTearDown</a> <a href=\"SuiteTearDown?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\n"+
-            "\t<div class=\"collapsable\" id=\"\">suiteTearDown</div>\n"+
-            "</div>\n"+
-            "<div class=\"teardown\">\n"+
-            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\n"+
-            "\t<a href=\"javascript:toggleCollapsable('');\">\n"+
-            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\n"+
-            "\t</a>\n"+
-            "&nbsp;<span class=\"meta\">Tear Down: <a href=\"TearDown\">.TearDown</a> <a href=\"TearDown?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\n"+
-            "\t<div class=\"collapsable\" id=\"\">teardown</div>\n"+
-            "</div>\n";
+    private String expectedResultForNonTestCase = "<div class=\"setup\">\r\n"+
+            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\r\n"+
+            "\t<a href=\"javascript:toggleCollapsable('');\">\r\n"+
+            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\r\n"+
+            "\t</a>\r\n"+
+            "&nbsp;<span class=\"meta\">Set Up: <a href=\"SetUp\">.SetUp</a> <a href=\"SetUp?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\r\n"+
+            "\t<div class=\"collapsable\" id=\"\">setup</div>\r\n"+
+            "</div>\r\n"+
+            "<div class=\"setup\">\r\n"+
+            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\r\n"+
+            "\t<a href=\"javascript:toggleCollapsable('');\">\r\n"+
+            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\r\n"+
+            "\t</a>\r\n"+
+            "&nbsp;<span class=\"meta\">Set Up: <a href=\"SuiteSetUp\">.SuiteSetUp</a> <a href=\"SuiteSetUp?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\r\n"+
+            "\t<div class=\"collapsable\" id=\"\">suiteSetUp</div>\r\n"+
+            "</div>\r\n"+
+            "<div class=\"setup\">\r\n"+
+            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\r\n"+
+            "\t<a href=\"javascript:toggleCollapsable('');\">\r\n"+
+            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\r\n"+
+            "\t</a>\r\n"+
+            "&nbsp;<span class=\"meta\">Set Up: <a href=\"SetUp\">.SetUp</a> <a href=\"SetUp?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\r\n"+
+            "\t<div class=\"collapsable\" id=\"\">setup</div>\r\n"+
+            "</div>\r\n"+
+            "<span class=\"meta\">variable defined: TEST_SYSTEM=slim</span><br/>the content!include -teardown <a href=\"TearDown\">.TearDown</a><br/><div class=\"teardown\">\r\n"+
+            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\r\n"+
+            "\t<a href=\"javascript:toggleCollapsable('');\">\r\n"+
+            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\r\n"+
+            "\t</a>\r\n"+
+            "&nbsp;<span class=\"meta\">Tear Down: <a href=\"SuiteTearDown\">.SuiteTearDown</a> <a href=\"SuiteTearDown?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\r\n"+
+            "\t<div class=\"collapsable\" id=\"\">suiteTearDown</div>\r\n"+
+            "</div>\r\n"+
+            "<div class=\"teardown\">\r\n"+
+            "\t<div style=\"float: right;\" class=\"meta\"><a href=\"javascript:expandAll();\">Expand All</a> | <a href=\"javascript:collapseAll();\">Collapse All</a></div>\r\n"+
+            "\t<a href=\"javascript:toggleCollapsable('');\">\r\n"+
+            "\t\t<img src=\"/files/images/collapsableOpen.gif\" class=\"left\" id=\"img\"/>\r\n"+
+            "\t</a>\r\n"+
+            "&nbsp;<span class=\"meta\">Tear Down: <a href=\"TearDown\">.TearDown</a> <a href=\"TearDown?edit&amp;redirectToReferer=true&amp;redirectAction=\">(edit)</a></span>\r\n"+
+            "\t<div class=\"collapsable\" id=\"\">teardown</div>\r\n"+
+            "</div>\r\n";
 
     @Before
     public void setUp() throws Exception {
         root = InMemoryPage.makeRoot("RooT");
         crawler = root.getPageCrawler();
-        testPage = addPage("TestPage", "!define TEST_SYSTEM {slim}\n" + "the content");
+        testPage = addPage("TestPage", "!define TEST_SYSTEM {slim}\r\n" + "the content");
         addPage("SetUp", "setup");
         addPage("TearDown", "teardown");
         addPage("SuiteSetUp", "suiteSetUp");
